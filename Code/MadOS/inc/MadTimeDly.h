5b8f80bc185aa27b69dcfed5999290012abfccd7
long568 <zandzx02cn@qq.com>
2016-12-30 17:14:34 +0800

    MadOS Cleanup
    Reupload Code

--- Code/MadOS/inc/MadTimeDly.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MAD_TIMEDLY__H__
-#define __MAD_TIMEDLY__H__
-
-#include "inc/MadGlobal.h"
-
-extern  void  madInitSysTick  (MadTim_t cnt);
-extern  void  madTimeDly      (MadTim_t timeCnt);
-
-#endif
--- Code/app/fatfs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FAT_FS__H__
-#define __FAT_FS__H__
-
-#include "string.h"
-#include "ff.h"
-
-extern void initMicroSD(void);
-
-#endif
