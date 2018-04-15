86d9195d7061038c562e83f4ad6ef3318f1955fd
long568 <zandzx02cn@qq.com>
2018-04-15 11:40:04 +0800

    MadOS v2.36 Debugging...

--- arch/stm32f10x/Arch/MadISR.h
+++ arch/stm32f10x/Arch/MadISR.h
@@ -3,6 +3,13 @@
 
 typedef void (*xIRQ_Handler)(void);
 
+#ifdef STM32F10X_CL
+#   define VECTOR_NUMBER 121
+#   define VECTOR_EX_OFS 16
+#else
+#   error Unknown MCU !
+#endif
+
 extern void HardFault_Handler(void);
 
 extern void madCopyVectorTab(void);
