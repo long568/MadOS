04b9b5694500ac0914e81ba29ab9187f635daece
long568 <zandzx02cn@qq.com>
2019-06-16 12:47:37 +0800

    MadOS v2.47.3
    1. Add config.sh to synchronize name of APP in the Makefile and c_cpp_properties.json.
    2. When using vscode, AutoLaunch-Extension is needed.
    3. Developer could work on different app, and keep the kernel code consistent at the same time.

--- tools/config.sh
+++ tools/config.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-source user_config.sh
+#!/bin/bash
+source $1/app_switcher.sh
 
 sed $"s/^export APP.*/export APP = $MADOS_WORKING_APP/" \
-    tools/temp.main.mk > main.mk
+    $1/tools/temp.main.mk > $1/main.mk
 sed $"s/\"\${workspaceFolder}\/app\/.*\"/\"\${workspaceFolder}\/app\/$MADOS_WORKING_APP\"/" \
-    tools/temp.c_cpp_properties.json > .vscode/c_cpp_properties.json
+    $1/tools/temp.c_cpp_properties.json > $1/.vscode/c_cpp_properties.json
 echo "MadOS is ready... Enjoy yourself!"
