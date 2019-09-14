53bdad9bf8434edd3f94743ae54757b1e13e47d2
long568 <zandzx02cn@qq.com>
2019-09-14 12:22:55 +0800

    MadOS v2.48.1
    1. Make __retarget_locks to inline.

--- library/Newlib/_sys_lock.c
+++ library/Newlib/_sys_lock.c
@@ -2,13 +2,15 @@
 #include <sys/lock.h>
 #include "MadOS.h"
 
-void __retarget_lock_init                 (_LOCK_T *lock) { *lock = madMutexCreate(); }
-void __retarget_lock_init_recursive       (_LOCK_T *lock) { *lock = madMutexCreateRecursive(); }
-void __retarget_lock_close                (_LOCK_T lock)  { madMutexDelete(&lock); }
-void __retarget_lock_close_recursive      (_LOCK_T lock)  { madMutexDelete(&lock); }
-void __retarget_lock_acquire              (_LOCK_T lock)  { madMutexWait(&lock, 0); }
-void __retarget_lock_acquire_recursive    (_LOCK_T lock)  { madMutexWait(&lock, 0); }
-int  __retarget_lock_try_acquire          (_LOCK_T lock)  { return MTRUE == madMutexCheck(&lock) ? 1 : -1; }
-int  __retarget_lock_try_acquire_recursive(_LOCK_T lock)  { return MTRUE == madMutexCheck(&lock) ? 1 : -1; }
-void __retarget_lock_release              (_LOCK_T lock)  { madMutexRelease(&lock); }
-void __retarget_lock_release_recursive    (_LOCK_T lock)  { madMutexRelease(&lock); }
+#define NL_MUTEX_CHEXK() ((MTRUE == madMutexCheck(&lock)) ? 1 : -1)
+
+inline void __retarget_lock_init                 (_LOCK_T *lock) { *lock = madMutexCreate(); }
+inline void __retarget_lock_init_recursive       (_LOCK_T *lock) { *lock = madMutexCreateRecursive(); }
+inline void __retarget_lock_close                (_LOCK_T lock)  { madMutexDelete(&lock); }
+inline void __retarget_lock_close_recursive      (_LOCK_T lock)  { madMutexDelete(&lock); }
+inline void __retarget_lock_acquire              (_LOCK_T lock)  { madMutexWait(&lock, 0); }
+inline void __retarget_lock_acquire_recursive    (_LOCK_T lock)  { madMutexWait(&lock, 0); }
+inline int  __retarget_lock_try_acquire          (_LOCK_T lock)  { return NL_MUTEX_CHEXK(); }
+inline int  __retarget_lock_try_acquire_recursive(_LOCK_T lock)  { return NL_MUTEX_CHEXK(); }
+inline void __retarget_lock_release              (_LOCK_T lock)  { madMutexRelease(&lock); }
+inline void __retarget_lock_release_recursive    (_LOCK_T lock)  { madMutexRelease(&lock); }
