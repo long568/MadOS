c8bcce3d2d7789994769f005ce6de62b5e88ad30
long568 <zandzx02cn@qq.com>
2020-04-14 22:24:51 +0800

    MadOS RC(20200414)

--- app_switcher.sh
+++ app_switcher.sh
@@ -2,4 +2,4 @@
 # export MADOS_WORKING_APP=LoNode
 # export MADOS_WORKING_APP=lesson001
 # export MADOS_WORKING_APP=lesson002
-export MADOS_WORKING_APP=lesson003
+export MADOS_WORKING_APP=PrjDataSwitcher
