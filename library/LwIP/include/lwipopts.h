f657c62809c83b88e82ed370190d142f352b61db
long568 <zandzx02cn@qq.com>
2020-05-05 20:55:40 +0800

    MadOS Debugging (20200505-2)

--- library/LwIP/include/lwipopts.h
+++ library/LwIP/include/lwipopts.h
@@ -65,8 +65,8 @@
 
 #define TCP_QUEUE_OOSEQ                 0
 #define TCP_MSS                         (ETH_PAYLOAD_LEN - 40)
-#define TCP_SND_BUF                     (2 * TCP_MSS)
-#define TCP_WND                         (4 * TCP_MSS)
+#define TCP_SND_BUF                     (6 * TCP_MSS)
+#define TCP_WND                         (6 * TCP_MSS)
 
 #define TCPIP_MBOX_SIZE                 4
 #define DEFAULT_RAW_RECVMBOX_SIZE       2
