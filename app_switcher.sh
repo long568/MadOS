834c9285d02d11b2b510e12d5d3f367c531a029b
long568 <zandzx02cn@qq.com>
2020-07-04 09:23:57 +0800

    MadOS Debugging 20200704

--- app_switcher.sh
+++ app_switcher.sh
@@ -1,7 +1,7 @@
 # export MADOS_WORKING_APP=LoKernel
-# export MADOS_WORKING_APP=LoNode
+export MADOS_WORKING_APP=LoNode
 # export MADOS_WORKING_APP=prjMM000
 # export MADOS_WORKING_APP=prjYCSD_ShengAn
-export MADOS_WORKING_APP=prjYCSD_Bosch
+# export MADOS_WORKING_APP=prjYCSD_Bosch
 # export MADOS_WORKING_APP=lesson001
 # export MADOS_WORKING_APP=lesson002
